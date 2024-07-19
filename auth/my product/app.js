fd91c37330ac95a13247506266520a640689622c
Aliraza <155419928+Aliraza708@users.noreply.github.com>
2024-07-20 03:24:18 +0500

    change

--- auth/my product/app.js
+++ auth/my product/app.js
@@ -84,11 +84,8 @@ async function getMyProduct(uid) {
                     </div>
                     <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                 </div>
+                <div class="flex  items-center justify-between">
                   <span class="text-3xl font-bold text-gray-900 ">$${productPrice}</span>
-                <div class="flex mt-3 items-center justify-between">
-                  
-                    <button id="${doc.id}"onclick ="cart(this)"
-                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">  ${(auth?.currentUser && product?.cartProductFire?.includes(auth?.currentUser.uid) ? "Carted" : "Add to Cart")}</button>
                 <button id="${doc.id}"onclick ="Delete(this)"
                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                        Delete 
